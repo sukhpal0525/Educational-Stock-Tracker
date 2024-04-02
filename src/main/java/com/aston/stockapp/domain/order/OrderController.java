10f30afb29877cbc18a42789901d6ef55a50daf3
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-04-02 23:13:57 +0100

    feature: order searching

--- src/main/java/com/aston/stockapp/domain/order/OrderController.java
+++ src/main/java/com/aston/stockapp/domain/order/OrderController.java
@@ -1,14 +1,22 @@
 package com.aston.stockapp.domain.order;
 
 import com.aston.stockapp.domain.portfolio.PortfolioService;
+import com.aston.stockapp.domain.transaction.Transaction;
 import com.aston.stockapp.domain.transaction.TransactionRepository;
 import com.aston.stockapp.user.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/orders")
@@ -18,16 +26,24 @@ public class OrderController {
     @Autowired private TransactionRepository transactionRepository;
 
     @GetMapping("")
-    public String getOrders(String symbol, Authentication authentication, Model model) {
-        // Check if the user is authenticated to populate user-related data
+    public String getOrders(@RequestParam(required = false) String ticker, Authentication authentication, Model model) {
         if (authentication != null && authentication.isAuthenticated()) {
-            Long userId = ((CustomUserDetails) authentication.getPrincipal()).getUser().getId();
-            model.addAttribute("ownsStock", portfolioService.ownsStock(symbol));
-            model.addAttribute("transactions", transactionRepository.findByUserId(userId));
-            model.addAttribute("quantity", portfolioService.getPortfolio(userId).getItems());
-            portfolioService.getCurrentUserBalance().ifPresent(balance -> model.addAttribute("balance", balance));
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            Long userId = userDetails.getUser().getId();
+
+            List<Transaction> transactions;
+            if (ticker != null && !ticker.isEmpty()) {
+                transactions = transactionRepository.findByUserIdAndStockTicker(userId, ticker.toUpperCase());
+            } else {
+                transactions = transactionRepository.findByUserId(userId);
+            }
+
+            model.addAttribute("transactions", transactions);
+            model.addAttribute("ticker", ticker);
+            return "orders";
+        } else {
+            return "redirect:/login";
         }
-        return "orders";
     }
 }
 
